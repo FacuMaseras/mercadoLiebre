5228be7f302bc65cdf93488f582db930479d4e8b
FacuMaseras <facundo.maseras@gmail.com>
2022-07-18 20:28:26 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+const port = 8080;
+
+const start = () => console.log('Starting server...');
+server.listen(port, start());
+
+const path = require('path'); //Su objetivo es unificar las rutas
+
+const public = path.join(__dirname, '../public');
+const statics = express.static(public);
+server.use(statics);
+
+server.get('/', function(req, res) {
+    let file = path.join(__dirname,'./views/home.html')
+    return res.sendFile(file);
+});
+
+server.get('/register', function(req, res) {
+    let file = path.join(__dirname,'./views/register.html')
+    return res.sendFile(file);
+});
+
+server.get('/login', function(req, res) {
+    let file = path.join(__dirname,'./views/login.html')
+    return res.sendFile(file);
+});
